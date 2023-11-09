260e26c4627cda1ddfdad221fe2eddaa279b240d
owengibson <114120616+owengibson@users.noreply.github.com>
2023-11-09 13:44:15 +1100

    Jellyfish now targetable by harpoons

--- Assets/Scripts/Enemy/Jellyfish.cs
+++ Assets/Scripts/Enemy/Jellyfish.cs
@@ -7,15 +7,8 @@ namespace CaveGame
     public class Jellyfish : Enemy, IDamageable
     {
         public EnemySO Enemy;
-        public Transform Target;
-
-        [SerializeField] private Animator _attackAnimator;
 
         private float _currentHealth;
-        private bool _hasBeenTriggered = false;
-        private bool _isPlayerInRange = false;
-
-        private List<IDamageable> _damageablesInsideRange = new();
 
         private void Start()
         {
@@ -33,55 +26,13 @@ namespace CaveGame
             return _currentHealth;
         }
 
-        private void Die()
+        public void Die()
         {
             foreach (var drop in Enemy.Drops)
             {
                 Instantiate(drop.Prefab, new Vector2(transform.position.x + Random.Range(-1f, 1f), transform.position.y + Random.Range(-1f, 1f)), Quaternion.identity);
             }
             Destroy(gameObject);
-        }
-
-        public void Explode()
-        {
-            foreach(var damageable in _damageablesInsideRange)
-            {
-                damageable.TakeDamage(Enemy.Damage);
-            }
-            Die();
-        }
-
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.CompareTag("Player"))
-            {
-                _hasBeenTriggered = true;
-                _isPlayerInRange = true;
-
-                _attackAnimator.SetTrigger("JellyfishTriggered");
-
-                _damageablesInsideRange.Add(collision.GetComponent<IDamageable>());
-            }
-
-            if (collision.TryGetComponent<IDamageable>(out var damageable))
-            {
-                _damageablesInsideRange.Add(damageable);
-            }
-            
-        }
-
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.CompareTag("Player"))
-            {
-                _isPlayerInRange = false;
-                _damageablesInsideRange.Remove(collision.GetComponent<IDamageable>());
-            }
-
-            if (collision.TryGetComponent<IDamageable>(out var damageable))
-            {
-                _damageablesInsideRange.Remove(damageable);
-            }
         }
     }
 }
--- Assets/Scripts/Enemy/JellyfishTrigger.cs
+++ Assets/Scripts/Enemy/JellyfishTrigger.cs
@@ -7,9 +7,51 @@ namespace CaveGame
     public class JellyfishTrigger : MonoBehaviour
     {
         [SerializeField] private Jellyfish _jellyfish;
-        public void TriggerExplosion()
+
+        private Animator _animator;
+        private List<IDamageable> _damageablesInsideRange = new();
+
+        private void Start()
         {
-            _jellyfish.Explode();
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Explode()
+        {
+            foreach (var damageable in _damageablesInsideRange)
+            {
+                damageable.TakeDamage(_jellyfish.Enemy.Damage);
+            }
+            _jellyfish.Die();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                _animator.SetTrigger("JellyfishTriggered");
+
+                _damageablesInsideRange.Add(collision.GetComponent<IDamageable>());
+            }
+
+            if (collision.TryGetComponent<IDamageable>(out var damageable))
+            {
+                _damageablesInsideRange.Add(damageable);
+            }
+
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                _damageablesInsideRange.Remove(collision.GetComponent<IDamageable>());
+            }
+
+            if (collision.TryGetComponent<IDamageable>(out var damageable))
+            {
+                _damageablesInsideRange.Remove(damageable);
+            }
         }
     }
 }
